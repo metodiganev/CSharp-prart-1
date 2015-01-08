767911851b19ec38c467d1dcbdd65fa1378f0475
metodi1 <metodi.ganev@gmail.com>
2015-01-08 21:08:44 +0200

    creat

---
+++ 8019849_MetodiGanev/Problem 6. Print Numbers/Problem 6. Print Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_6.Print_Numbers
+{
+    class PrintNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+        }
+    }
+}
