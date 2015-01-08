133df9f66bf17e8567ddaca6dc9eebd774de0afb
metodi1 <metodi.ganev@gmail.com>
2015-01-08 21:36:52 +0200

    creat

---
+++ 8019849_MetodiGanev/Problem 7. Print First and Last Name/Problem 7. Print First and Last Name/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_7.Print_First_and_Last_Name
+{
+    class PrintFirstandLastName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metodi");
+            Console.WriteLine("Ganev");
+        }
+    }
+}
---
+++ 8019849_MetodiGanev/Problem 8. Square Root/Problem 8. Square Root/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_8.Square_Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("The square root of 12345 is: {0}", Math.Sqrt(12345));
+        }
+
+    }
+}
