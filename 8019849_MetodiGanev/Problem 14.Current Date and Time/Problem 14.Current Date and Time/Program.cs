b81086cf245b231c97e70ddcc7f3a83aa67e01a4
metodi1 <metodi.ganev@gmail.com>
2015-01-09 18:47:12 +0200

    creat

---
+++ 8019849_MetodiGanev/Problem 14.Current Date and Time/Problem 14.Current Date and Time/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_14.Current_Date_and_Time
+{
+    //Create a console application that prints the current date and time. Find out how in Internet.
+    class dataTime
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
